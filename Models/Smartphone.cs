d4fb06aafe19ab751b8fc90b75b40f40a8a78e2f
Caique Krajezar <caiquersa@hotmail.com>
2025-06-30 21:30:27 -0300

    Resolvendo os Problemas do Desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,19 @@
 namespace DesafioPOO.Models
 {
+    // Classe abstrata Smartphone
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -21,6 +26,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo(string nome);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "192645387", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Nubank");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("987654321", "iPhone 15", "954382617", 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+
+        
+        Console.WriteLine($"\nNúmero do Nokia: {nokia.Numero}");
+        Console.WriteLine($"Número do iPhone: {iphone.Numero}");
+    }
+}
